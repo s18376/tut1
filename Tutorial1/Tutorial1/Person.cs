704033c2aa067ddc6c55b99a37dcd09b5518c7f6
s18376 <s18376@pjwstk.edu.pl>
2020-03-06 15:36:16 +0100

    class

---
+++ Tutorial1/Tutorial1/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tutorial1
+{
+    public class Person
+    {
+        private string _firstName;
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; }
+        }
+        public string LastName { get; set; }
+    }
+}
---
+++ Tutorial1/Tutorial1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Tutorial1
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            //int? g = null;
+            //int b = true;
+            //string str = "small s is an allias";
+
+            //int age = 5;
+
+            //Console.WriteLine($"I am {5} years old.");
+            //Console.WriteLine("Hello World!");
+
+            var url = @"https://www.pja.edu.pl";
+            using (var httpClient = new HttpClient())
+            {
+
+                //var response = await httpClient.GetAsync(url);
+                using (var response = await httpClient.GetAsync(url))
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+
+                    //response.Dispose();
+
+                    var regex = new Regex("[a-z]+[a-z+-9]*@[a-z]+\\.[a-z]+", RegexOptions.IgnoreCase);
+                    var matches = regex.Matches(content);
+                    foreach (var item in matches)
+                    {
+                        Console.WriteLine(item.ToString());
+                    }
+                }
+            }
+        }
+    }
+}
